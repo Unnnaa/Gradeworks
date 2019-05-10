750166011e4834bd16c6574643135bede7e86387
Unnnaa <Hellop2k@yahoo.com>
2019-05-09 18:55:32 -0700

    update

--- app/src/main/java/com/example/grade/MainActivity.kt
+++ app/src/main/java/com/example/grade/MainActivity.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    // var java = 0
-    // var network = 0
-    // var database = 0
+     var java = 0
+     var network = 0
+     var database = 0
     // var total = 0
     // var ave = 0
 
@@ -35,33 +35,28 @@ class MainActivity : AppCompatActivity() {
         var ave: Int
 
 
-        var java = editTextJava.text.toString().toInt()
-        var net = editTextNetwork.text.toString().toInt()
-        var database = editTextDatabase.text.toString().toInt()
+
 
 
         buttonTotal.setOnClickListener {
-            total = java + net + database
-            editTextTotal.setText(total)
+
+            java = editTextJava.text.toString().toInt()
+            network = editTextNetwork.text.toString().toInt()
+            database = editTextDatabase.text.toString().toInt()
+
+            total = java + network + database
+            textViewTotal.text = total.toString()
         }
 
         buttonAve.setOnClickListener {
             ave = total / 3
-            editTextAve.setText(ave)
-            if (ave >= 90) {
-                ggrade.text = "A"
-            } else if (ave >= 80){
-                ggrade.text = "B"
-            }
-            else if (ave >= 70){
-                ggrade.text = "C"
-            }
-            else if (ave >=60){
-                ggrade.text = "D"
-            }
-            else {
-                ggrade.text = "F"
-
+            textViewAve.text = ave.toString()
+            when {
+                ave >= 90 -> ggrade.text = "A"
+                ave >= 80 -> ggrade.text = "B"
+                ave >= 70 -> ggrade.text = "C"
+                ave >=60 -> ggrade.text = "D"
+                else -> ggrade.text = "F"
             }
 
         }
